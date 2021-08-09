711745a20cf05816d2a191e6020125ff7b3a45d9
d3rp-jsx <15928886+derp-jsx@users.noreply.github.com>
2021-08-09 10:38:00 +0200

    Initial release

---
+++ client.lua
@@ -0,0 +1,32 @@
+local crouched = false
+
+RegisterCommand('crouch', function()
+	if DoesEntityExist(PlayerPedId()) and not IsEntityDead(PlayerPedId()) and not IsPedInAnyVehicle(PlayerPedId(), 1) and not IsPauseMenuActive()  then 
+		RequestAnimSet("move_ped_crouched")
+		
+		while not HasAnimSetLoaded("move_ped_crouched") do 
+			Citizen.Wait(100)
+		end 
+
+		if crouched then 
+			ResetPedMovementClipset(PlayerPedId(), 0.25)
+			crouched = false 
+		else
+			SetPedMovementClipset(PlayerPedId(), "move_ped_crouched", 0.25 )
+			crouched = true 
+		end 
+
+		RemoveAnimSet("move_ped_crouched")
+	else
+		crouched = false
+	end
+end, false)
+
+RegisterKeyMapping('crouch', 'Crouch', 'keyboard', 'lcontrol')
+
+Citizen.CreateThread(function()
+  while true do
+    Citizen.Wait(0)
+		DisableControlAction(0, 36, true)
+  end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'd3rp-jsx'
+description 'Better Crouch'
+version '1.0.0'
+
+client_script 'client.lua'
